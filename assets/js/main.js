3a6f49795f09a7e4d9691acc0c21ab7ec02b0a94
Ángel Guerrero <lasantiagoguerrero@gmail.com>
2020-11-22 21:12:13 -0600

    Agregando funcionalidad de los botones

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,53 @@ const vm = new Vue({
     // Selecciona la primera vista a elegir, en base al radio button.
     // 
     // Si es el modelo segmentado o el modelo recomendado.
-    modeloSeleccionado: ''
-  }
+    modeloSeleccionado: '',
+
+    // 
+    // Botones para el comportamiento del progreso del usuario.
+    // Son las opciones que se iteran para mostrar los pasos
+    // que está siguiendo el usuario.
+    pasos: [
+      { title: 'Modelos', active: true },
+      { title: 'Sociodemográficos', active: false },
+      { title: 'Comportamiento del cliente', active: false },
+      { title: 'Geograficos', active: false },
+      { title: 'Datos', active: false },
+      { title: 'Formulario', active: false }
+    ],
+
+    // 
+    // Es la vista que está viendo el usuario
+    // Corresponde a el número de paso actual.
+    // 
+    pasoSeleccionado: 0
+  },
+
+  methods: {
+    onClickSeleccionarPaso(index) {
+      this.removeClasses()
+
+      let i = 0
+      for (; i <= index; i++) {
+        this.pasos[i].active = true
+      }
+      this.pasoSeleccionado = i - 1
+    },
+
+    removeClasses() {
+      this.pasos.map(item => { item.active = false })
+    },
+
+    prevSection() {
+      if ((this.pasoSeleccionado - 1) < 0) return
+
+      this.onClickSeleccionarPaso(this.pasoSeleccionado - 1)
+    },
+
+    nextSection() {
+      if ((this.pasoSeleccionado + 1) >= this.pasos.length) return
+
+      this.onClickSeleccionarPaso(this.pasoSeleccionado + 1)
+    }
+  },
 })
